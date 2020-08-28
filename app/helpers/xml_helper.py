1dc9f8f3aaec8482338c3f4f11e02493bc0484c8
Mattias.Poppe <poppe.mattias@gmail.com>
2020-08-28 15:57:51 +0200

    Allow construction of the outgoing XML requests

---
+++ app/helpers/xml_helper.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from uuid import uuid4
+
+from lxml import etree
+
+from app.helpers.events_parser import SubtitleEvent
+
+NAMESPACE_TNS = "http://www.vrt.be/mig/viaa"
+NSMAP = {"tns": NAMESPACE_TNS}
+REQUESTOR = "VIAA"
+
+
+def construct_request(event: SubtitleEvent) -> str:
+    """ Constructs the makeSubtitleAvailableRequest XML. """
+    root = etree.Element(f"{{{NAMESPACE_TNS}}}makeSubtitleAvailableRequest", nsmap=NSMAP)
+    etree.SubElement(root, f"{{{NAMESPACE_TNS}}}requestor").text = f"{REQUESTOR}"
+    etree.SubElement(root, f"{{{NAMESPACE_TNS}}}correlationId").text = str(uuid4())
+    etree.SubElement(root, f"{{{NAMESPACE_TNS}}}id").text = f"{event.media_id}"
+    etree.SubElement(root, f"{{{NAMESPACE_TNS}}}destinationPath").text = (
+        f"s3://mam-collaterals/{event.destination_path_type()}/{event.media_id}/"
+    )
+    etree.SubElement(root, f"{{{NAMESPACE_TNS}}}otType").text = f"{event.ot_type}"
+
+    return etree.tostring(
+        root, pretty_print=True, encoding="UTF-8", xml_declaration=True
+    )
---
+++ tests/helpers/test_xml_helper.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from io import BytesIO
+
+from lxml import etree
+
+from app.helpers.events_parser import SubtitleEvent
+from app.helpers.xml_helper import construct_request, NSMAP
+from tests.resources.resources import construct_filename, load_resource
+
+BASE_XPATH = "/tns:makeSubtitleAvailableRequest"
+
+
+def test_construct_request_open():
+    event = SubtitleEvent(load_resource("openOtAvailableEvent.xml"))
+    xml = etree.parse(BytesIO(construct_request(event)))
+    # Assert correct values in the XML
+    assert _xml_value(xml, "/tns:requestor") == "VIAA"
+    assert len(_xml_value(xml, "/tns:correlationId")) == 36
+    assert _xml_value(xml, "/tns:id") == "media_id_open"
+    assert _xml_value(xml, "/tns:otType") == "OPEN"
+    assert _xml_value(xml, "/tns:destinationPath") == (
+        "s3://mam-collaterals/openOt/media_id_open/"
+    )
+
+    # Assert validness according to schema
+    assert _xml_valid_schema(xml)
+
+
+def test_construct_request_closed():
+    event = SubtitleEvent(load_resource("closedOtAvailableEvent.xml"))
+    xml = etree.parse(BytesIO(construct_request(event)))
+    # Assert correct values in the XML
+    assert _xml_value(xml, "/tns:requestor") == "VIAA"
+    assert len(_xml_value(xml, "/tns:correlationId")) == 36
+    assert _xml_value(xml, "/tns:id") == "media_id_closed"
+    assert _xml_value(xml, "/tns:otType") == "CLOSED"
+    assert _xml_value(xml, "/tns:destinationPath") == (
+        "s3://mam-collaterals/closedOt/media_id_closed/"
+    )
+
+    # Assert validness according to schema
+    assert _xml_valid_schema(xml)
+
+
+def _xml_value(xml, xpath: str) -> str:
+    return xml.xpath(f"{BASE_XPATH}{xpath}", namespaces=NSMAP)[0].text
+
+
+def _xml_valid_schema(xml) -> bool:
+    # Load in XML schema
+    schema = etree.XMLSchema(file=construct_filename("makeAvailableRequests.xsd"))
+    # Assert validness according to schema
+    return schema.validate(xml)
