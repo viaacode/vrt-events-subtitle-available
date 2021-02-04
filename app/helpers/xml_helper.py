915cd007f18121aa1fe4aaac9dae78561916999b
DriesDD <dries.dedecker@meemoo.be>
2021-02-04 14:58:27 +0100

    DEV-1241 Use hex instead of string for correlation ID

--- app/helpers/xml_helper.py
+++ app/helpers/xml_helper.py
@@ -16,7 +16,7 @@ def construct_request(event: SubtitleEvent) -> str:
     """ Constructs the makeSubtitleAvailableRequest XML. """
     root = etree.Element(f"{{{NAMESPACE_TNS}}}makeSubtitleAvailableRequest", nsmap=NSMAP)
     etree.SubElement(root, f"{{{NAMESPACE_TNS}}}requestor").text = f"{REQUESTOR}"
-    etree.SubElement(root, f"{{{NAMESPACE_TNS}}}correlationId").text = str(uuid4())
+    etree.SubElement(root, f"{{{NAMESPACE_TNS}}}correlationId").text = uuid4().hex
     etree.SubElement(root, f"{{{NAMESPACE_TNS}}}id").text = f"{event.media_id}"
     etree.SubElement(root, f"{{{NAMESPACE_TNS}}}destinationPath").text = (
         f"mam-collaterals/{event.destination_path_type()}/{event.media_id}"
